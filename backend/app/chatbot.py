ac49bed21c4867f980bf82ee0942cdf6ed8f632a
Derek <someflydays@gmail.com>
2025-04-21 17:41:27 -0400

    improve graph logic

--- backend/app/chatbot.py
+++ backend/app/chatbot.py
@@ -23,8 +23,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Set up LLM
-llm = ChatOpenAI(model="gpt-4o", temperature=0)
+# Set up LLMs
+internal_llm = ChatOpenAI(model="gpt-4o", temperature=0)
+deterministic_streaming_llm = ChatOpenAI(model="gpt-4o", streaming=True, temperature=0)
+creative_streaming_llm = ChatOpenAI(model="gpt-4o", streaming=True, temperature=0.6)
 
 # Define API request schema
 class ChatRequest(BaseModel):
@@ -39,50 +41,94 @@ class ChatResponse(BaseModel):
 # Define graph state schema
 class MessagesState(TypedDict):
     messages: Annotated[list[AnyMessage], add_messages]
+    mode: str
+    timeframe: str
+    token: str
 
+"""
+Define nodes and router functions
+"""
 def input_handler(state: MessagesState) -> dict:
-    pass
+    # TODO: Add logic to process user input
+    return state # For now, just return state
     
-def router(state: MessagesState) -> str:
+def query_router(state: MessagesState) -> str:
     mode = state.get("mode")
     if mode == "recap":
-        return "generate_recap_query"
+        return "create_recap_query"
     elif mode == "foresight":
-        return "generate_foresight_query"
+        return "create_foresight_query"
     else:
-        return "generate_direct_response"
+        return "create_general_prompt"
     
-def generate_recap_query(state: MessagesState) -> dict:
+def create_recap_query(state: MessagesState) -> dict:
+    # TODO: Call internal_llm
     pass
 
-def generate_foresight_query(state: MessagesState) -> dict:
+def create_foresight_query(state: MessagesState) -> dict:
+    # TODO: Call internal_llm
+    pass
+    
+def create_general_prompt(state: MessagesState) -> dict:
+    # TODO: Call internal_llm
     pass
     
 def run_search(state: MessagesState) -> dict:
+    # TODO: Use SERP tool
     pass
     
-def generate_serped_response(state: MessagesState) -> dict:
+def response_router(state: MessagesState) -> str:
+    mode = state.get("mode")
+    if mode == "recap":
+        return "generate_deterministic_serped_response"
+    elif mode == "foresight":
+        return "generate_creative_serped_response"
+    else:
+        return "generate_general_response"
+    
+def generate_deterministic_serped_response(state: MessagesState) -> dict:
+    # TODO: Call deterministic_streaming_llm
     pass
     
-def generate_direct_response(state: MessagesState) -> dict:
+def generate_creative_serped_response(state: MessagesState) -> dict:
+    # TODO: Call creative_streaming_llm
+    pass
+    
+def generate_general_response(state: MessagesState) -> dict:
+    # TODO: Call deterministic_streaming_llm
     pass
 
+# Create graph
 builder = StateGraph(MessagesState)
+
+# Add nodes
 builder.add_node("input_handler", input_handler)
-builder.add_node("generate_recap_query", generate_recap_query)
-builder.add_node("generate_foresight_query", generate_foresight_query)
+builder.add_node("create_recap_query", create_recap_query)
+builder.add_node("create_foresight_query", create_foresight_query)
+builder.add_node("create_general_prompt", create_general_prompt)
 builder.add_node("run_search", run_search)
-builder.add_node("generate_serped_response", generate_serped_response)
-builder.add_node("generate_direct_response", generate_direct_response)
+builder.add_node("generate_general_response", generate_general_response)
+builder.add_node(
+    "generate_deterministic_serped_response",
+    generate_deterministic_serped_response
+)
+builder.add_node(
+    "generate_creative_serped_response",
+    generate_creative_serped_response
+)
 
+# Add edges
 builder.add_edge(START, "input_handler")
-builder.add_conditional_edges("input_handler", router)
-builder.add_edge("generate_recap_query", "run_search")
-builder.add_edge("generate_foresight_query", "run_search")
-builder.add_edge("run_search", "generate_serped_response")
-builder.add_edge("generate_serped_response", END)
-builder.add_edge("generate_direct_response", END)
+builder.add_conditional_edges("input_handler", query_router)
+builder.add_edge("create_recap_query", "run_search")
+builder.add_edge("create_foresight_query", "run_search")
+builder.add_edge("create_general_prompt", "generate_general_response")
+builder.add_edge("generate_general_response", END)
+builder.add_conditional_edges("run_search", response_router)
+builder.add_edge("generate_deterministic_serped_response", END)
+builder.add_edge("generate_creative_serped_response", END)
 
+# Compile graph
 graph = builder.compile()
 
 # API chat endpoint
